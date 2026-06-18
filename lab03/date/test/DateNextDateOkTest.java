71531104aa89f2f92d6316d31c1033db48e35b23
Steven Wu <97002253+wu0x51@users.noreply.github.com>
2026-06-17 23:29:53 -0400

    Add test covering Date.java line 109 (partially, 1/16 missed)

--- lab03/date/test/DateNextDateOkTest.java
+++ lab03/date/test/DateNextDateOkTest.java
@@ -41,6 +41,8 @@ public class DateNextDateOkTest
     params.add(new Integer[] { 1901, 7, 31, 1901, 8, 1 });
     params.add(new Integer[] { 3453, 1, 31, 3453, 2, 1 });
     params.add(new Integer[] { 3456, 12, 31, 3457, 1, 1 });
+    params.add(new Integer[] { 2000, 2, 28, 2000, 2, 29 });
+    params.add(new Integer[] { 1900, 2, 28, 1900, 3, 1 });
     return params;
   }
 
