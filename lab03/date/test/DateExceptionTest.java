52ab06a1a81aea08e7efa20cc106381cf92c6d17
Steven Wu <97002253+wu0x51@users.noreply.github.com>
2026-06-17 21:35:43 -0400

    Add test covering Date.java line 49

--- lab03/date/test/DateExceptionTest.java
+++ lab03/date/test/DateExceptionTest.java
@@ -36,6 +36,7 @@ public class DateExceptionTest {
         params.add(new Integer[] { -1, 10, 20, 0, 0, 0 });
         params.add(new Integer[] { 1458, 15, 12, 0, 0, 0 });
         params.add(new Integer[] { 1975, 6, -50, 0, 0, 0 });
+        params.add(new Integer[] { 2026, 1, 32, 0, 0, 0 });
         return params;
     }
 
